9ff29d43e8e37281ed24f571c3ef4318f2eaa158
nadine35 <nadhegean@yahoo.fr>
2022-06-16 15:30:04 +0200

    modif fichier

--- back/controllers/post.controller.js
+++ back/controllers/post.controller.js
@@ -10,7 +10,49 @@ module.exports.readPost = (req, res) => {
     else console.log("Error to get data :" + err);
   }).sort({ createdAt: -1 }); //affiche message du + récent au plus ancien
 };
+module.exports.createPost = async (req, res) => {
+  let fileName;
 
+  if (req.file !== null) {
+    try {
+      if (
+        req.file.detectedMimeType != "image/jpg" &&
+        req.file.detectedMimeType != "image/png" &&
+        req.file.detectedMimeType != "image/jpeg"
+      )
+        throw Error("invalid file");
+
+      if (req.file.size > 500000) throw Error("max size");
+    } catch (err) {
+      const errors = uploadErrors(err);
+      return res.status(201).json({ errors });
+    }
+    fileName = req.body.posterId + Date.now() + ".jpg";
+
+    await pipeline(
+      req.file.stream,
+      fs.createWriteStream(
+        `${__dirname}/../client/public/uploads/posts/${fileName}`
+      )
+    );
+  }
+
+  const newPost = new postModel({
+    posterId: req.body.posterId,
+    message: req.body.message,
+    picture: req.file !== null ? "./uploads/posts/" + fileName : "",
+    video: req.body.video,
+    likers: [],
+    comments: [],
+  });
+
+  try {
+    const post = await newPost.save();
+    return res.status(201).json(post);
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
 
 
 
